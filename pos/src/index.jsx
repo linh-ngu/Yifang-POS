c77c48ffa1f814e2c0723c63b1e4a8e4f12d2f80
Linh Nguyen <linh.n565@gmail.com>
2023-10-26 19:58:17 -0500

    package

--- pos/src/index.jsx
+++ pos/src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const myFirstElement = <h1>Hello React!</h1>
+// const myFirstElement = <h1>Hello React!</h1>
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App/>)
