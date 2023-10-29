214afc59419617f76cb5b07cc518964f4d4f3de0
Linh Nguyen <linh.n565@gmail.com>
2023-10-29 17:43:09 -0500

    reorganized login

--- pos/src/App.js
+++ pos/src/App.js
@@ -1,37 +1,25 @@
 // import logo from './logo.svg';
 import './App.css';
-import { Login } from "./Login";
-import "./App.css"
-import Navbar from "./components/Navbar";
+import { Login } from "./pages/Login";
 import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import Footer from './components/Footer';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
 
   return (
     <div className="App">
-      <Login />
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      <BrowserRouter> 
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home/>} />
-          {/* <Route path="/about" element={<h1>About</h1>} /> */}
-        </Routes>
-      </BrowserRouter>
+        <BrowserRouter>
+          <Navbar />
+          <div className='page-wrap'>
+            <Routes>
+              <Route path="/" element={<Home/>} />
+              <Route path="/login" element={<Login />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
     </div>
   );
 }
