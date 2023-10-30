e160c36cd0f6686a5f202f0ee1664adf590af941
LilyTTT <yushantang4@gmail.com>
2023-10-30 14:09:36 -0500

    Added skeleton for drink customization (sugarcane)

--- pos/src/Cashier/Cashier.jsx
+++ pos/src/Cashier/Cashier.jsx
@@ -1,53 +1,73 @@
-import Category from "./order_home";
 import React from "react";
-import "./Cashier.css"
+import "../styles/Cashier.css"
 
 class Cashier extends React.Component {
     constructor(props) {
         super(props);
+        this.drink = "huh";
         this.home = 
                 <div class = "grid-container-in">
                     <div>
-                        <Category name = "Teas"/>
+                        <button class = "button"  onClick={() => this.changeCurr(this.teas)}><p class = "text">Teas</p></button>
                     </div>
                     <div>
-                        <Category name = "Brown Sugar"/>
+                        <button class = "button"  onClick={() => this.changeCurr(this.bwsg)}><p class = "text">Brown Sugar</p></button>
                     </div>
                     <div>
-                        <Category name = "Milk Tea"/>
+                        <button class = "button"  onClick={() => this.changeCurr(this.milkTea)}><p class = "text">Milk Tea</p></button>
                     </div>
                     <div>
-                        <Category name = "Fruits"/>
+                        <button class = "button"  onClick={() => this.changeCurr(this.fruits)}><p class = "text">Fruits</p></button>
                     </div>
                     <div>
-                        <Category name = "Fresh Sugarcane" onClick={this.changeCurr(this.sugarcane)}/>
+                        <button class = "button"  onClick={() => this.changeCurr(this.sugarcane)}><p class = "text">Fresh Sugarcane</p></button>
+                        {/* <Category name = "Fresh Sugarcane" onClick={() => this.changeCurr(this.sugarcane)}/> */}
                     </div>
                     <div>
-                        <Category name = "Fresh Taro/Red Bean"/>
+                        <button class = "button"  onClick={() => this.changeCurr(this.taroBean)}><p class = "text">Fresh Taro/Red Bean</p></button>
                     </div>
                     <div>
-                        <Category name = "Traditional"/>
+                        <button class = "button"  onClick={() => this.changeCurr(this.traditional)}><p class = "text">Traditional</p></button>
                     </div>
                 </div>;
 
-        this.sugarcane =
-            <div class = "grid-container-in">
-                    <div>
-                        <Category name = "sugarcane mountain tea"/>
-                    </div>
-                    <div>
-                        <Category name = "sugarcane lemon monutain tea"/>
-                    </div>
-                </div>;
         this.state = {
             staffId: props.staffId,
-            curr: this.home
-
+            curr: this.home,
         };
+
+        this.customization = (
+            <div>
+              <p>Customization for: {this.drink}</p>
+              {/* Add additional customization content here */}
+            </div>
+        );
+
+        this.sugarcane =(
+            <div class = "grid-container-in">
+                <div>
+                    <button class = "button"  onClick={() => this.changeCurr(this.customization, "sugarcane mountain tea")}><p class = "text">sugarcane mountain tea</p></button>
+                </div>
+                <div>
+                    <button class = "button"  onClick={() => this.changeCurr(this.customization, "sugarcane lemon mountain tea")}><p class = "text">sugarcane lemon mountain tea</p></button>
+                </div>
+            </div>
+        );
     }
 
-    changeCurr = (props) => {
-        this.setState({props});
+    changeCurr = (newContent, newDrink = "None") => {
+        if(newDrink != "None"){
+            this.drink = newDrink;
+            this.customization = (
+                <div>
+                <p>Customization for: {this.drink}</p>
+                {/* Add additional customization content here */}
+                </div>
+            );
+            newContent = this.customization;
+        }
+
+        this.setState({ curr: newContent });
     }
 
     render() {
