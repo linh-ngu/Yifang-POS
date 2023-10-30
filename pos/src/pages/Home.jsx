bad8c009f390d92c122c16c1e1f7eddf7ed13b8f
Linh Nguyen <linh.n565@gmail.com>
2023-10-29 19:16:00 -0500

    added home view

--- pos/src/pages/Home.jsx
+++ pos/src/pages/Home.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Video from '../assets/Yifang.mov'
+import '../styles/Home.css'
 
 function Home() {
   return (
-    <div></div>
+    <div className='home'>
+      <div className="overlay"></div>
+      <video src={Video} autoPlay loop muted/>
+      <div className='video-text'>
+        <h1>Yi Fang Taiwan Fruit Tea</h1>
+        <h3>Authentic  ·  Organic  ·  Fresh</h3>
+      </div>
+    </div>
   )
 }
 
