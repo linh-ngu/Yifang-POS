998eaec299cc0b19f3e15b99b17582e3bc190047
kimcchen <kimberlykchen@gmail.com>
2023-10-30 17:50:42 -0500

    made a few edits

--- pos/src/pages/Login.jsx
+++ pos/src/pages/Login.jsx
@@ -1,28 +1,56 @@
-import React, { useState } from "react";
-import "../styles/Login.css";
+// import React, { useState } from "react";
+// import "../styles/Login.css";
+// import { GoogleLogin } from 'react-google-login';
 
-export const Login = () => {
-    const [staffid, setStaffID] = useState('');
+// const clientId = "659412670449-bqubbteq6sk1dfk903m8mdh7onu59j4r.apps.googleusercontent.com";
 
-    const handleSubmit = (e) => {
-        // preventDefault makes sure form cannot be submitted without correct login info
-        e.preventDefault();
-        console.log(staffid);
-    }
+// function Login() {
 
-    return (
-        <div className="auth-form-container">
-            <form className="login-form" onSubmit={handleSubmit}>
-                <label htmlFor="staffid">Staff ID</label>
-                <input 
-                    value={staffid} 
-                    onChange={(e) => setStaffID(e.target.value)} 
-                    type="text" 
-                    placeholder="Your Staff ID number" 
-                    id="staffid" 
-                    name="staffid" />
-                <button type="submit">Log In</button>           
-            </form>
-        </div>
-    )
-}
+//     const onSuccess = (res) => {
+//         console.log("Login Success. Current user: ", res.profileObj);
+//     }
+//     const onFailure = (res) => {
+//         console.log("Login Failed. res: ", res);
+//     }
+
+//     return (
+//         <div id="signInButton">
+//             <GoogleLogin
+//                 clientId={clientId}
+//                 buttonText="Login"
+//                 onSuccess={onSuccess}
+//                 onFailure={onFailure}
+//                 cookiePolicy={'single-host-origin'}
+//                 isSignedIn={true}
+//             />
+//         </div>
+//     )
+// }
+
+// // export const Login = () => {
+// //     const [staffid, setStaffID] = useState('');
+
+// //     const handleSubmit = (e) => {
+// //         // preventDefault makes sure form cannot be submitted without correct login info
+// //         e.preventDefault();
+// //         console.log(staffid);
+// //     }
+
+// //     return (
+// //         <div className="auth-form-container">
+// //             <form className="login-form" onSubmit={handleSubmit}>
+// //                 <label htmlFor="staffid">Staff ID</label>
+// //                 <input 
+// //                     value={staffid} 
+// //                     onChange={(e) => setStaffID(e.target.value)} 
+// //                     type="text" 
+// //                     placeholder="Your Staff ID number" 
+// //                     id="staffid" 
+// //                     name="staffid" />
+// //                 <button type="submit">Log In</button>           
+// //             </form>
+// //         </div>
+// //     )
+// // }
+
+// export default Login
