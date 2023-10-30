8764c5e6d2f2384c7d82d70187f0e915fb5f63e5
kimcchen <kimberlykchen@gmail.com>
2023-10-29 21:44:48 -0500

    ugly but functioning translate api

--- pos/src/pages/Home.jsx
+++ pos/src/pages/Home.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+// import React from 'react'
 import Video from '../assets/Yifang.mov'
 import '../styles/Home.css'
+import translateText from '../apirequests/GoogleTranslate';
+import React, { useState } from "react";
 
 function Home() {
+  const [inputText, setInputText] = useState('');
+  const [targetLanguage, setTargetLanguage] = useState('es'); // Default: Spanish
+
+  const handleTranslate = async () => {
+    if (inputText) {
+      const translatedText = await translateText(inputText, targetLanguage);
+      console.log(translatedText)
+      // Do something with the translatedText, e.g., display it on the page.
+    }
+  };
+
   return (
     <div className='home'>
       <div className="overlay"></div>
@@ -10,7 +23,21 @@ function Home() {
       <div className='video-text'>
         <h1>Yi Fang Taiwan Fruit Tea</h1>
         <h3>Authentic  ·  Organic  ·  Fresh</h3>
-        <button type="text">Translate</button>    
+        {/* adding translate button */}
+        <input
+        type="text"
+        value={inputText}
+        onChange={(e) => setInputText(e.target.value)}
+        />
+        <select
+          value={targetLanguage}
+          onChange={(e) => setTargetLanguage(e.target.value)}
+        >
+          <option value="es">Spanish</option>
+          <option value="fr">French</option>
+          {/* Add more language options */}
+        </select>
+        <button onClick={handleTranslate}>Translate</button>   
       </div>
     </div>
   )
