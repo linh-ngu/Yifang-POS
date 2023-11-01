cca9efa8233f89bf7c12b3c9e806e5e84e7d9663
Nishka Mittal <nishka06@tamu.edu>
2023-10-31 20:03:14 -0500

    inital manager side

---
+++ pos/src/Manager/Menu.jsx
@@ -0,0 +1,87 @@
+import React, { Component } from 'react';
+import jdbcpostgreSQL from './jdbcpostgreSQL';
+import './MenuStyles.css';
+
+class Menu extends Component {
+  constructor() {
+    super();
+    this.state = {
+      drink_id_list: [],
+      name_list: [],
+      price_list: [],
+      ingredients_list: [],
+    };
+  }
+
+  componentDidMount() {
+    this.loadTable();
+  }
+
+  loadTable() {
+    // Fetch data from your backend and update state
+    // Example:
+    // const data = fetchDataFromDatabase();
+    // this.setState({
+    //   drink_id_list: data.drink_id_list,
+    //   name_list: data.name_list,
+    //   price_list: data.price_list,
+    //   ingredients_list: data.ingredients_list,
+    // });
+  }
+
+  render() {
+    return (
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>Drink ID</th>
+              <th>Name</th>
+              <th>Price</th>
+              <th>Ingredients List</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.drink_id_list.map((drinkId, index) => (
+              <tr key={index}>
+                <td>{drinkId}</td>
+                <td>{this.state.name_list[index]}</td>
+                <td>{this.state.price_list[index]}</td>
+                <td>{this.state.ingredients_list[index]}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <div>
+          <button onClick={this.addDrinks}>Add</button>
+          <button onClick={this.removeDrinks}>Remove</button>
+          <button onClick={this.changeName}>Name</button>
+          <button onClick={this.changePrice}>Price</button>
+          <button onClick={this.changeIngredients}>Ingredients</button>
+        </div>
+      </div>
+    );
+  }
+
+  addDrinks = () => {
+    // Implement the add drink logic here
+  };
+
+  removeDrinks = () => {
+    // Implement the remove drink logic here
+  };
+
+  changeName = () => {
+    // Implement the change name logic here
+  };
+
+  changePrice = () => {
+    // Implement the change price logic here
+  };
+
+  changeIngredients = () => {
+    // Implement the change ingredients logic here
+  };
+}
+
+export default Menu;
