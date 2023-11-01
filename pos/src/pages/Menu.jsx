94e2dc904950191f721a1905f0062e982d98aeca
Linh Nguyen <linh.n565@gmail.com>
2023-11-01 18:06:33 -0500

    added menu view

--- pos/src/pages/Menu.jsx
+++ pos/src/pages/Menu.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import Cards from '../components/Cards'
+import Food from '../components/Food'
 import '../styles/Menu.css'
 
 function Menu() {
   return (
     <div className='menu'>
         <div className="menu-wrap">
-            <p>hi</p>
+            {/* <Cards /> */}
+            <Food />
         </div>
     </div>
   )
