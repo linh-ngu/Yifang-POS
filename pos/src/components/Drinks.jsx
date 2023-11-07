f5e8499fb8c0ca664d47fecc34cee9f6784aae5b
Linh Nguyen <linh.n565@gmail.com>
2023-11-06 23:00:23 -0600

    customization screen for ordering

--- pos/src/components/Drinks.jsx
+++ pos/src/components/Drinks.jsx
@@ -1,41 +1,68 @@
 import React, { useState} from 'react'
-import {data} from '../assets/data'
+import {drinksData} from '../assets/drinks'
+import {toppingsData} from '../assets/toppings'
+import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
 
 const Drink = () => {
     
-    const [orders, setDrinks] = useState(data)
+    const [drinks, setDrinks] = useState(drinksData)
+    const [toppings] = useState(toppingsData)
 
-    const filterType = (category) => {
-        setDrinks(
-            data.filter((item) => {
-                return item.category === category
+    const [activeFilter, setActiveFilter] = useState('All');
+
+    const handleFilter = (category) => {
+        if (category === 'All') {
+          setDrinks(drinksData);
+        } else {
+          setDrinks(
+            drinksData.filter((item) => {
+              return item.category === category;
             })
-        );
-    };
+          );
+        }
+        setActiveFilter(category);
+      };
     
     //customization stuff
     const Customization = ({ item, onClose }) => {
         if (!item) return null;
       
         return (
-          <div className="fixed inset-0 bg-black bg-opacity-50 backdrop-blur-sm flex justify-center items-center">
-            <div className="bg-white rounded-lg">
-              {/* Add your customization fields here */}
-              <img src={item.image} alt={item.name} 
-              className='h-[300px] w-[590px] object-cover rounded-t-lg'/>
-              <div className='px-8'>
-                <h2 className="text-xl font-bold pt-4 pb-2">{item.name}</h2>
-                <p className='pb-4'>add ingredients</p>
-                <hr className='solid'></hr>
-                <h3 className='py-4'>Add Toppings (Additional)</h3>
-                <div className='grid grid-cols-2 gap-3 pt-4'>
-                    
+          <div className="fixed inset-0 bg-black bg-opacity-50 backdrop-blur-sm flex justify-center items-center z-50">
+                <div className="relative bg-white rounded-lg overflow-y-scroll h-3/4">
+                    <img src={item.image} alt={item.name} 
+                    className='h-[300px] w-[590px] object-cover rounded-t-lg'/>
+                    <div className='px-8'>
+                        <h2 className="text-xl font-bold pt-4 pb-2">{item.name}</h2>
+                        <p className='pb-4'>add ingredients</p>
+                        <hr className='solid'></hr>
+                        <h3 className='py-4 font-bold'>Add Toppings (Additional)</h3>
+                        <div className='grid grid-cols-2 gap-3 py-2'>
+                            {toppings.map((item, index) => (
+                                <div key={index} className='border shadow-lg rounded-lg hover:scale-105 duration-300'>
+                                    <div className='flex justify-between px-2 py-4'>
+                                        <p className='font-medium'>{item.name}</p>
+                                    </div>
+                                </div>
+                            ) )}
+                        </div>
+                        <hr className='solid'></hr>
+                        <h3 className='py-4 font-bold'>Special Instructions</h3>
+                        <textarea className='border rounded-lg w-full h-24'></textarea>
+                    </div>
+                    <button onClick={onClose} className='bg-white absolute top-2 right-3 m-2 rounded-full p-2'><CloseOutlinedIcon/></button>
+                    <div className='border bg-white h-[72px] sticky bottom-0 mt-2 flex justify-between items-center'>
+                        <div className='flex justify-center items-center m-4'>
+                            <button className='m-2 cursor-pointer'>-</button>
+                            <p>
+                                <span className='bg-blue-200 text-black p-1 rounded-full'>{item.price}</span> {/*change price to count*/}
+                            </p>
+                            <button className='m-2 cursor-pointer'>+</button>
+                        </div>
+                        <button className='bg-blue-500 rounded-full h-10 w-full m-6'>Add to Cart</button>
+                    </div>
                 </div>
-              </div>
-              <button onClick={onClose} className="border px-4 py-2 mt-4">Close</button>
-              <button>Add to Cart</button>
             </div>
-          </div>
         );
       };
 
@@ -56,21 +83,21 @@ const Drink = () => {
                 <div> {/*filter type*/}
                     <p className='font-bold text-gray-700'>Filter Type</p>
                     <div className='flex'>
-                        <button onClick={()=> setDrinks(data)} className='m-1 border-yellow-600 text-yellow-600 hover:bg-yellow-600 hover:text-white'>All</button>
-                        <button onClick={()=> filterType('Tea')} className='m-1 border-yellow-600 text-yellow-600 hover:bg-yellow-600 hover:text-white'>Tea</button>
-                        <button onClick={()=> filterType('Brown Sugar')} className='m-1 border-yellow-600 text-yellow-600 hover:bg-yellow-600 hover:text-white'>Brown Sugar</button>
-                        <button onClick={()=> filterType('Milk Tea')}className='m-1 border-yellow-600 text-yellow-600 hover:bg-yellow-600 hover:text-white'>Milk Tea</button>
-                        <button onClick={()=> filterType('Fruits')}className='m-1 border-yellow-600 text-yellow-600 hover:bg-yellow-600 hover:text-white'>Fruits</button>
-                        <button onClick={()=> filterType('Fresh Sugarcane')}className='m-1 border-yellow-600 text-yellow-600 hover:bg-yellow-600 hover:text-white'>Fresh Sugarcane</button>
-                        <button onClick={()=> filterType('Fresh Taro/Red Bean')}className='m-1 border-yellow-600 text-yellow-600 hover:bg-yellow-600 hover:text-white'>Fresh Taro/Red Bean</button>
-                        <button onClick={()=> filterType('Traditional')}className='m-1 border-yellow-600 text-yellow-600 hover:bg-yellow-600 hover:text-white'>Traditional</button>
-                        <button onClick={()=> filterType('Seasonal')}className='m-1 border-yellow-600 text-yellow-600 hover:bg-yellow-600 hover:text-white'>Seasonal</button>
+                    <button onClick={() => handleFilter('All')} className={`rounded-lg m-2 p-2 border border-yellow-600 ${activeFilter === 'All' ? 'bg-yellow-600 text-white' : 'text-yellow-600 hover:bg-yellow-600 hover:text-white'}`}>All</button>
+                        <button onClick={() => handleFilter('Tea')} className={`rounded-lg m-2 p-2 border border-yellow-600 ${activeFilter === 'Tea' ? 'bg-yellow-600 text-white' : 'text-yellow-600 hover:bg-yellow-600 hover:text-white'}`}>Tea</button>
+                        <button onClick={() => handleFilter('Brown Sugar')} className={`rounded-lg m-2 p-2 border border-yellow-600 ${activeFilter === 'Brown Sugar' ? 'bg-yellow-600 text-white' : 'text-yellow-600 hover:bg-yellow-600 hover:text-white'}`}>Brown Sugar</button>
+                        <button onClick={() => handleFilter('Milk Tea')} className={`rounded-lg m-2 p-2 border border-yellow-600 ${activeFilter === 'Milk Tea' ? 'bg-yellow-600 text-white' : 'text-yellow-600 hover:bg-yellow-600 hover:text-white'}`}>Milk Tea</button>
+                        <button onClick={() => handleFilter('Fruits')} className={`rounded-lg m-2 p-2 border border-yellow-600 ${activeFilter === 'Fruits' ? 'bg-yellow-600 text-white' : 'text-yellow-600 hover:bg-yellow-600 hover:text-white'}`}>Fruits</button>
+                        <button onClick={() => handleFilter('Fresh Sugarcane')} className={`rounded-lg m-2 p-2 border border-yellow-600 ${activeFilter === 'Fresh Sugarcane' ? 'bg-yellow-600 text-white' : 'text-yellow-600 hover:bg-yellow-600 hover:text-white'}`}>Fresh Sugarcane</button>
+                        <button onClick={() => handleFilter('Fresh Taro/Red Bean')} className={`rounded-lg m-2 p-2 border border-yellow-600 ${activeFilter === 'Fresh Taro/Red Bean' ? 'bg-yellow-600 text-white' : 'text-yellow-600 hover:bg-yellow-600 hover:text-white'}`}>Fresh Taro/Red Bean</button>
+                        <button onClick={() => handleFilter('Traditional')} className={`rounded-lg m-2 p-2 border border-yellow-600 ${activeFilter === 'Traditional' ? 'bg-yellow-600 text-white' : 'text-yellow-600 hover:bg-yellow-600 hover:text-white'}`}>Traditional</button>
+                        <button onClick={() => handleFilter('Seasonal')} className={`rounded-lg m-2 p-2 border border-yellow-600 ${activeFilter === 'Seasonal' ? 'bg-yellow-600 text-white' : 'text-yellow-600 hover:bg-yellow-600 hover:text-white'}`}>Seasonal</button>
                     </div>
                 </div>
             </div>
 
             <div className='grid grid-cols-2 lg:grid-cols-3 gap-6 pt-4'> {/*display food cards*/}
-                {orders.map((item, index) => (
+                {drinks.map((item, index) => (
                     <div key={index} className='border shadow-lg rounded-lg hover:scale-105 duration-300' onClick={() => openModal(item)}>
                         <img src={item.image} alt={item.name}
                         className='w-full h-[400px] object-cover rounded-t-lg' />
