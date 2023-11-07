b0568333bf8ab32c7c23466a2d7a28381a96616d
Linh Nguyen <linh.n565@gmail.com>
2023-11-07 15:09:58 -0600

    changed to tailwind

--- pos/src/components/Navbar.jsx
+++ pos/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import Logo from '../assets/yiFangLogo.png'
+import Logo from '../assets/YifangLogo.png'
 import { Link } from 'react-router-dom'
 import MenuIcon from '@mui/icons-material/Menu';
 import '../styles/Navbar.css'
@@ -10,10 +10,10 @@ function Navbar() {
         setOpenLinks(!openLinks)
     }
     return (
-        <div className='navbar'>
-            <div className='leftSide' id={openLinks ? "open" : "close"}>
+        <div className='navbar w-full h-[150px] flex justify-between absolute z-10 backdrop-blur-sm'>
+            <div className='flex items-center justify-center' id={openLinks ? "open" : "close"}>
             <Link to="/">
-                <img src={Logo} alt='Logo'/>
+                <img src={Logo} alt='Logo' className='w-[150px]'/>
             </Link>
                 <div className='hiddenLinks'>
                     <Link to='/menu'>Menu</Link>
