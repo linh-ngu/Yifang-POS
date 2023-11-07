73e0d195f8857769a93bf825b8129c9479985a1e
Linh Nguyen <linh.n565@gmail.com>
2023-11-06 18:26:18 -0600

    ordering screen style

--- pos/src/components/Drinks.jsx
+++ pos/src/components/Drinks.jsx
@@ -23,9 +23,14 @@ const Drink = () => {
               {/* Add your customization fields here */}
               <img src={item.image} alt={item.name} 
               className='h-[300px] w-[590px] object-cover rounded-t-lg'/>
-              <div className='px-8 py-6'>
-                <h2 className="text-xl font-bold">{item.name}</h2>
-                <p>Customize your drink...</p>
+              <div className='px-8'>
+                <h2 className="text-xl font-bold pt-4 pb-2">{item.name}</h2>
+                <p className='pb-4'>add ingredients</p>
+                <hr className='solid'></hr>
+                <h3 className='py-4'>Add Toppings (Additional)</h3>
+                <div className='grid grid-cols-2 gap-3 pt-4'>
+                    
+                </div>
               </div>
               <button onClick={onClose} className="border px-4 py-2 mt-4">Close</button>
               <button>Add to Cart</button>
