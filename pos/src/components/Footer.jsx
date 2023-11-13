88e57d974d47d3563a8519f86673d4f0e019bbd0
Linh Nguyen <linh.n565@gmail.com>
2023-11-13 17:44:37 -0600

    moved weather and translate to footer

--- pos/src/components/Footer.jsx
+++ pos/src/components/Footer.jsx
@@ -1,13 +1,50 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import Logo from '../assets/YifangLogo.png'
+import getWeather from '../apirequests/AccuWeather'
 
 function Footer() {
+
+  const googleTranslateElementInit = () => {
+    new window.google.translate.TranslateElement(
+      {
+        pageLanguage: "en",
+        autoDisplay: false
+      },
+      "google_translate_element"
+    );
+  };
+
+  useEffect(() => {
+    var addScript = document.createElement("script");
+    addScript.setAttribute(
+      "src",
+      "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
+    );
+    document.body.appendChild(addScript);
+    window.googleTranslateElementInit = googleTranslateElementInit;
+  }, []);
+
+  const [weatherText, setWeatherText] = useState('');
+  const [translatedText] = useState('');
+
+  const handleWeather = async() => {
+    const weather = await getWeather();
+    setWeatherText(weather.Headline.Text + " - Min: " + weather.DailyForecasts[0].Temperature.Minimum.Value
+    + "F, Max: " + weather.DailyForecasts[0].Temperature.Maximum.Value + "F");
+    console.log(weather);
+  }
+
   return (
-    <div className='flex bottom-0 justify-center items-center h-[150px] p-0 m-0'>
+    <div className='flex bottom-0 h-[150px] p-0 m-0 justify-center items-center'>
       <Link to="/">
           <img src={Logo} alt='Logo' className='w-[150px]'/>
       </Link>
+      <p id="google_translate_element" className='border-4'>{translatedText}</p>
+      <div className='max-w-[200px]'>
+        <button className='bg-[#ADD8E6] p-[10px] rounded-xl' onClick={handleWeather}>Weather</button>
+        <p>{weatherText}</p>
+      </div>
     </div>
   )
 }
--- pos/src/pages/Home.jsx
+++ pos/src/pages/Home.jsx
@@ -1,53 +1,16 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import Video from '../assets/Yifang.mov'
 import '../styles/Home.css'
-import getWeather from '../apirequests/AccuWeather';
 import Footer from '../components/Footer';
 
 function Home() {
-  const googleTranslateElementInit = () => {
-    new window.google.translate.TranslateElement(
-      {
-        pageLanguage: "en",
-        autoDisplay: false
-        
-      },
-      "google_translate_element"
-    );
-  };
-
-  useEffect(() => {
-    var addScript = document.createElement("script");
-    addScript.setAttribute(
-      "src",
-      "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
-    );
-    document.body.appendChild(addScript);
-    window.googleTranslateElementInit = googleTranslateElementInit;
-  }, []);
- 
-  const [weatherText, setWeatherText] = useState('');
-  const [translatedText] = useState('');
-
-  const handleWeather = async() => {
-    const weather = await getWeather();
-    setWeatherText(weather.Headline.Text + " - Min: " + weather.DailyForecasts[0].Temperature.Minimum.Value
-    + "F, Max: " + weather.DailyForecasts[0].Temperature.Maximum.Value + "F");
-    console.log(weather);
-  }
-
   return (
-    <div className='flex flex-col justify-center items-center'>
+    <div className='flex flex-col justify-center items-center p-0 m-0'>
       <div className="absolute w-full bg-black bg-opacity-50 h-screen top-0"></div>
-      <video src={Video} autoPlay loop muted className='top-0 object-cover h-screen'/>
-      <div className='video-text'>
-        <h1>Yi Fang Taiwan Fruit Tea</h1>
-        <h3>Authentic  ·  Organic  ·  Fresh</h3>
-        <div>
-          <p id="google_translate_element" className='p-4'>{translatedText}</p>
-          <button className='weather-button' onClick={handleWeather}>Weather</button>
-          <p>{weatherText}</p>
-        </div>
+      <video src={Video} autoPlay loop muted className='top-0 object-cover h-screen w-screen'/>
+      <div className='absolute text-center text-white transform -translate-y-10'>
+        <h1 className='text-7xl p-4'>Yi Fang Taiwan Fruit Tea</h1>
+        <h3 className='text-3xl p-4'>Authentic  ·  Organic  ·  Fresh</h3>
       </div>
       <Footer />
     </div>
