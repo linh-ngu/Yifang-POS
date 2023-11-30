bb911ed2fe86565b99522eae576304f612b06b5e
Linh Nguyen <linh.n565@gmail.com>
2023-11-29 23:20:32 -0600

    added popups to menu tab

--- pos/src/components/Menu.jsx
+++ pos/src/components/Menu.jsx
@@ -1,91 +1,10 @@
-import React from 'react';
-// import jdbcpostgreSQL from './jdbcpostgreSQL';
-import { useEffect } from 'react';
-import { useState } from 'react';
-// import './MenuStyles.css';
-// import '../styles/IngredientStyles.css';
+import React, { useEffect, useState } from 'react';
+import AddItem from './menuTableButtons/addItem';
+import RemoveItem from './menuTableButtons/removeItem';
+import ChangeName from './menuTableButtons/changeName';
+import ChangePrice from './menuTableButtons/changePrice';
+import ChangeIngredients from './menuTableButtons/changeIngredients';
 
-// class Menu extends Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       drink_id_list: [],
-//       name_list: [],
-//       price_list: [],
-//       ingredients_list: [],
-//     };
-//   }
-
-//   componentDidMount() {
-//     this.loadTable();
-//   }
-
-//   loadTable() {
-//     // Fetch data from your backend and update state
-//     // Example:
-//     // const data = fetchDataFromDatabase();
-//     // this.setState({
-//     //   drink_id_list: data.drink_id_list,
-//     //   name_list: data.name_list,
-//     //   price_list: data.price_list,
-//     //   ingredients_list: data.ingredients_list,
-//     // });
-//   }
-
-//   render() {
-//     return (
-      // <div>
-      //   <table>
-      //     <thead>
-      //       <tr>
-      //         <th>Drink ID</th>
-      //         <th>Name</th>
-      //         <th>Price</th>
-      //         <th>Ingredients List</th>
-      //       </tr>
-      //     </thead>
-      //     <tbody>
-      //       {this.state.drink_id_list.map((drinkId, index) => (
-      //         <tr key={index}>
-      //           <td>{drinkId}</td>
-      //           <td>{this.state.name_list[index]}</td>
-      //           <td>{this.state.price_list[index]}</td>
-      //           <td>{this.state.ingredients_list[index]}</td>
-      //         </tr>
-      //       ))}
-      //     </tbody>
-      //   </table>
-      //   <div>
-      //     <button onClick={this.addDrinks}>Add</button>
-      //     <button onClick={this.removeDrinks}>Remove</button>
-      //     <button onClick={this.changeName}>Name</button>
-      //     <button onClick={this.changePrice}>Price</button>
-      //     <button onClick={this.changeIngredients}>Ingredients</button>
-      //   </div>
-      // </div>
-//     );
-//   }
-
-  // addDrinks = () => {
-  //   // Implement the add drink logic here
-  // };
-
-  // removeDrinks = () => {
-  //   // Implement the remove drink logic here
-  // };
-
-  // changeName = () => {
-  //   // Implement the change name logic here
-  // };
-
-  // changePrice = () => {
-  //   // Implement the change price logic here
-  // };
-
-  // changeIngredients = () => {
-  //   // Implement the change ingredients logic here
-  // };
-// }
 const Menu = () => {
   const[menu, setMenu] = useState([]);
 
@@ -106,44 +25,51 @@ const Menu = () => {
       getMenu();
   },[]);
 
-  const addDrinks = () => {
-    console.log("hi");
+  const [showModel, setShowModel] = useState("");
+
+  const openModel = (val) => {
+      setShowModel(val);
+  };
+
+  const closeModel = () => {
+      setShowModel("");
   };
   
   return (
-    <div className="ingredient-container">
-            <table className="ingredient-table">
-                <thead>
-                    <tr>
-                      <th>Drink ID</th>
-                      <th>Name</th>
-                      <th>Price</th>
-                      <th>Ingredients List</th>
-                    </tr>
-                </thead>
-                <tbody>
-                {menu.map((base_drink, index) => (
-                  <tr key={index}>
-                      <td>{base_drink.base_id}</td>
-                      <td>{base_drink.name}</td>
-                      <td>{base_drink.price}</td>
-                      <td>{base_drink.list_ingredients}</td>
-                  </tr>
-                ))}
-                </tbody>
-            </table>
-              <button onClick={addDrinks}>Add</button>
-              <button>Remove</button>
-              <button>Name</button>
-              <button>Price</button>
-              <button>Ingredients</button>
-        </div>
-    // <div>
-    //   {menu.map(base_drink => (
-    //     <p>{base_drink.base_id} | {base_drink.name} | {base_drink.price} | {base_drink.list_ingredients}</p>
-    //   ))}
-    // </div>
-
+    <div className=''>
+      <table className='w-full text-center border border-black mb-4'>
+          <thead>
+            <tr>
+              <th className='p-1'>Drink ID</th>
+              <th className='p-1'>Name</th>
+              <th className='p-1'>Price</th>
+              <th className='p-1'>Ingredients List</th>
+            </tr>
+          </thead>
+          <tbody>
+          {menu.map((base_drink, index) => (
+            <tr key={index}>
+              <td className='p-1'>{base_drink.base_id}</td>
+              <td className='p-1'>{base_drink.name}</td>
+              <td className='p-1'>{base_drink.price}</td>
+              <td className='p-1'>{base_drink.list_ingredients}</td>
+            </tr>
+          ))}
+          </tbody>
+      </table>
+      <div className='flex justify-center'>
+        <button className='border border-black p-2 m-4' onClick={() => openModel("addItem")}>Add</button>
+        <button className='border border-black p-2 m-4' onClick={() => openModel("removeItem")}>Remove</button>
+        <button className='border border-black p-2 m-4' onClick={() => openModel("changeName")}>Name</button>
+        <button className='border border-black p-2 m-4' onClick={() => openModel("changePrice")}>Price</button>
+        <button className='border border-black p-2 m-4' onClick={() => openModel("changeIngredients")}>Ingredients</button>
+      </div>
+      {showModel === 'addItem' && <AddItem onClose={closeModel} />}
+      {showModel === 'removeItem' && <RemoveItem onClose={closeModel} />}
+      {showModel === 'changeName' && <ChangeName onClose={closeModel} />}
+      {showModel === 'changePrice' && <ChangePrice onClose={closeModel} />}
+      {showModel === 'changeIngredients' && <ChangeIngredients onClose={closeModel} />}
+    </div>
   )
 };
 
