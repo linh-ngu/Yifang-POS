f6ce2ca699a21bede7601d6e10ffb6ada2bf9bac
Nishka Mittal <nishka06@tamu.edu>
2023-12-03 14:22:20 -0600

    order history queries added

--- pos/src/components/orderHistoryTableButtons/slowDay.jsx
+++ pos/src/components/orderHistoryTableButtons/slowDay.jsx
@@ -1,13 +1,45 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
-const slowDay = ({onClose}) => {
+const SlowDay = ({ onClose }) => {
+  const [slowDaysData, setSlowDaysData] = useState(null);
+
+  useEffect(() => {
+    const fetchSlowDaysData = async () => {
+      try {
+        const response = await fetch('http://localhost:5000/order/slowdays');
+        const data = await response.json();
+
+        setSlowDaysData(data);
+      } catch (error) {
+        console.error('Error fetching slow days data:', error.message);
+      }
+    };
+
+    fetchSlowDaysData();
+  }, []);
+
   return (
     <div className='fixed inset-0 bg-black bg-opacity-50 backdrop-blur-sm flex justify-center items-center z-50 p-8'>
-        <div className='relative bg-white rounded-lg h-[500px] w-[500px]'>
-            <button onClick={onClose}>Cancel</button>
-        </div>
+      <div className='relative bg-white rounded-lg h-[500px] w-[500px]'>
+        {slowDaysData ? (
+          <div>
+            <p>Slow Days:</p>
+            <ul>
+              {slowDaysData.map((day, index) => (
+                <li key={index}>
+                  <p>Order Day: {day.order_day}</p>
+                  <p>Total Order Amount: {day.total_order_amount}</p>
+                </li>
+              ))}
+            </ul>
+          </div>
+        ) : (
+          <p>Loading...</p>
+        )}
+        <button onClick={onClose}>Cancel</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default slowDay
+export default SlowDay;
--- pos/src/components/orderHistoryTableButtons/totalOrders.jsx
+++ pos/src/components/orderHistoryTableButtons/totalOrders.jsx
@@ -1,13 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
-const totalOrders = ({onClose}) => {
+const TotalOrders = ({ onClose }) => {
+  const [totalOrders, setTotalOrders] = useState(null);
+
+  useEffect(() => {
+    const fetchTotalOrders = async () => {
+      try {
+        const response = await fetch('/order/totalorders');
+        const data = await response.json();
+
+        // Directly set the value without checking array and length
+        setTotalOrders(data[0]?.row_count);
+      } catch (error) {
+        console.error('Error fetching total orders:', error.message);
+      }
+    };
+
+    fetchTotalOrders();
+  }, []);
+
+  const handleCancel = () => {
+    onClose();
+  };
+
   return (
     <div className='fixed inset-0 bg-black bg-opacity-50 backdrop-blur-sm flex justify-center items-center z-50 p-8'>
-        <div className='relative bg-white rounded-lg h-[500px] w-[500px]'>
-            <button onClick={onClose}>Cancel</button>
-        </div>
+      <div className='relative bg-white rounded-lg h-[200px] w-[200px]'>
+        <p>Total Orders: {totalOrders}</p>
+        <button onClick={handleCancel}>Cancel</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default totalOrders
+export default TotalOrders;
