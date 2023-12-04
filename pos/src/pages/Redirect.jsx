8e488e6afd524777a5bee4043f4ba89e222e5ca2
LilyTTT <yushantang4@gmail.com>
2023-12-04 09:22:47 -0600

    Added aria labels for Login, Order and Redirect

--- pos/src/pages/Redirect.jsx
+++ pos/src/pages/Redirect.jsx
@@ -9,8 +9,8 @@ function Redirect() {
         <div className='absolute top-[150px] w-full'>
             <h1 className='font-bold text-4xl text-center'>Redirect Page</h1>
             <div className='flex justify-center items-center p-4'>
-                <button className='p-2 border m-2 rounded-sm shadow-sm' onClick={() => navigate('/manager')}>Manager</button>
-                <button className='p-2 border m-2 rounded-sm shadow-sm' onClick={() => navigate('/cashier')}>Cashier</button>
+                <button aria-label="Manager" className='p-2 border m-2 rounded-sm shadow-sm' onClick={() => navigate('/manager')}>Manager</button>
+                <button aria-label="Cashier" className='p-2 border m-2 rounded-sm shadow-sm' onClick={() => navigate('/cashier')}>Cashier</button>
             </div>
         </div>
     )
